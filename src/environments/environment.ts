73bfd6e086925aa2c0115e46d7592db0be043f3d
“amkumar”072 <amkumar072@gmail.com>
2021-05-09 10:14:26 +0100

    firebase config added

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDnykD29cqdymppfnhuUQUPURoK7QqBov4',
+    authDomain: 'plasma-donar-covid.firebaseapp.com',
+    databaseURL: 'https://plasma-donar-covid-default-rtdb.asia-southeast1.firebasedatabase.app',
+    projectId: 'plasma-donar-covid',
+    storageBucket: 'plasma-donar-covid.appspot.com',
+    messagingSenderId: '23122939358',
+    appId: '1:23122939358:web:417ead8657f8cd8fcca14f',
+    measurementId: 'G-VEYWVHRR7T'
+  }
 };
 
 /*
